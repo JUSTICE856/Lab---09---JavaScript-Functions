89f833788e04bbe52d53cac55eeba58e737e7a31
JUSTIC856 <justice.ndukwu@mail.citytech.cuny.edu>
2021-04-12 16:03:31 -0400

    Completed

---
+++ pluralize.py
@@ -0,0 +1,6 @@
+def pluralize(word): { # 1. word is passed as input PARAMETER into the function...
+    return word + "s" # ...word is RETURNED  in place at the function call with processed data.
+
+
+userword = input("Word please:")
+print(pluralize(userword)); # userword is a variable that's passed as an AGRUMENT to the function call.
